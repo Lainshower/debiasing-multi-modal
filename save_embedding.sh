c124941759e22f326971d46dfc78aaf5dbe9f948
sjinu96 <pakpaa@gmail.com>
2023-06-08 18:16:22 +0900

    modify readme

--- save_embedding.sh
+++ save_embedding.sh
@@ -1,8 +1,5 @@
-# python clip_inference.py --data_dir data --dataset waterbirds --embedding_dir embeddings --embeddings --predictions --save_path minor_pred/waterbirds.pt
+# celeba 
+python clip_inference.py --data_dir data --dataset celeba --embedding_dir embeddings_unnormalized --save --split all --backbone RN50
 
-list="RN50" #  RN101 RN50x4 ViT-B/32"
- 
-for var in $list
-do
-  python clip_inference.py --data_dir data --dataset celeba --embedding_dir embeddings_unnormalized --save --split all --backbone $var
-done
+# waterbirds
+python clip_inference.py --data_dir data --dataset waterbirds --embedding_dir embeddings_unnormalized --save --split all --backbone RN50
